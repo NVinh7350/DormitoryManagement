a4d4a74ddfff882fdf3b954396c57a85b2145f9b
Nguyen Van Vinh <vanvinhqn7350@gmail.com>
2023-03-30 11:41:02 +0700

    create project

---
+++ src/test/java/com/example/dormitorymanagement/DormitoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dormitorymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
