4c9912e2280b09be901242b10c0b43ed53304c78
NghiaPham <ngph01012k1@gmail.com>
2023-04-05 12:41:40 +0700

    config security

--- src/main/java/com/example/dormitorymanagement/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/dormitorymanagement/security/SpringSecurityConfiguration.java
@@ -1,10 +1,16 @@
 package com.example.dormitorymanagement.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration {
     @Bean
@@ -17,4 +23,22 @@ public class SpringSecurityConfiguration {
                 .formLogin(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("khanh")
+                .password("123456")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("thanh")
+                .password("123456")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+
 }
